e614a8972aeef9f6c1fd3fb30cc0cfed2bdd499f
kongmenglor <lorxx504@umn.edu>
2023-02-02 10:37:16 -0600

    html and css is completed. Just need to connect js

---
+++ mini_calendar/index.js
@@ -0,0 +1,16 @@
+const monthName = document.getElementById("month-name");
+const dayName = document.getElementById("day-name");
+const dayNumber = document.getElementById("day-number");
+const year = document.getElementById("year");
+const previous = document.getElementById("previous");
+const next = document.getElementById("next");
+
+
+const date = new Date();
+console.log(date.getFullYear())
+console.log(date.getDay())
+
+
+// function calculate_Calender() {
+
+// }
